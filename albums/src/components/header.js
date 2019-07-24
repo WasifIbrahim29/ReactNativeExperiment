26303a079461780ca41f8be7f8085c05acc392a4
Wasif Ibrahim <wasif.Ibrahim29@gmail.com>
2019-07-23 20:32:48 -0800

    third commit

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => (
-        <Text>Wasif!</Text>
-);
+const Header = () => {
+
+    const { textStyle, viewStyle } = styles;
+
+    <View>
+    <Text style={textStyle}>Wasif!</Text>
+    </View>
+};
+
+const styles={
+
+    viewStyle:{
+        backgroundColor: 'F8F8F8';
+    }
+
+    textStyle: {
+        fontSize: 20
+    }
+
+};
 
 export default Header;
